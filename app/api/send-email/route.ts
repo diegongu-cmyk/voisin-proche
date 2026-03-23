7c501ccf79a54b56d9f98adb7fb58d197e3f295c
Diego <diegongu@gmail.com>
2026-03-23 13:11:26 +0100

    Add email API endpoint

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -5,14 +5,12 @@ const resend = new Resend('re_BiWgvYh3_2nHN8V2BAojzCfQcnVhZVFD8')
 
 export async function POST(request: Request) {
   const { to, subject, html } = await request.json()
-  
   const { data, error } = await resend.emails.send({
     from: 'Voisin Proche <onboarding@resend.dev>',
     to,
     subject,
     html
   })
-
   if (error) return NextResponse.json({ error }, { status: 400 })
   return NextResponse.json({ success: true, data })
 }
