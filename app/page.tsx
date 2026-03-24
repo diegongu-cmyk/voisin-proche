3bc8b9c84221f42db606eaeaeef94d92152c949b
Diego <diegongu@gmail.com>
2026-03-24 19:07:13 +0100

    Fix accompagnement slider image

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ const sliderServices = [
   {
     titre: "Accompagnement",
     prix: "Depuis 12€",
-    image: "https://images.unsplash.com/photo-1569398034126-476e3a9e3808?w=600",
+    image: "https://images.unsplash.com/photo-1559839734-2b71ea197ec2?w=600",
   },
   {
     titre: "Courses et commissions",
